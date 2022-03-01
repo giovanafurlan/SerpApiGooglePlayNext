21f3b816ddc6ae7610ee090b173d89e4938e15ae
Giovana Furlan <giovananelofurlan@gmail.com>
2022-03-01 16:06:43 -0300

    Pacotes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,49 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
+
+const activeLabelStyles = {
+  transform: 'scale(0.85) translateY(-24px) translateX(-10px)',
+}
+
+export const theme = extendTheme({
+  components: {
+    Form: {
+      variants: {
+        floating: {
+          container: {
+            _focusWithin: {
+              label: {
+                ...activeLabelStyles,
+              },
+            },
+            'input:not(:placeholder-shown) + label, .chakra-select__wrapper + label':
+            {
+              ...activeLabelStyles,
+            },
+            label: {
+              top: 0,
+              left: 0,
+              zIndex: 2,
+              position: 'absolute',
+              backgroundColor: 'white',
+              pointerEvents: 'none',
+              mx: 3,
+              px: 1,
+              my: 2,
+            },
+          },
+        },
+      },
+    },
+  },
+})
